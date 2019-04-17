c994a4363e097949290bb32dee1b82a882f1548a
John Nelson <me@johnrnelson.com>
2019-04-17 10:48:06 -0700

    fix refresh bug

--- APIServer/APIServer.js
+++ APIServer/APIServer.js
@@ -14,7 +14,7 @@
 
 //Yes.. it's a global!!! 
 global.SERVER = {
-    Version: '1.10.15',
+    Version: '1.10.16',
     CERTS: {
         //Change this for your own domain!
         path: 'demo.tektology.com'
