ea2ae1ec378c16194707b208195daf467deafb84
John Nelson <me@johnrnelson.com>
2019-04-22 14:54:36 -0700

    more app pref work

--- docs/debug/src/AppPrefs.js
+++ docs/debug/src/AppPrefs.js
@@ -1,14 +1,22 @@
-
+/*
+    Simple manager for prefs the user might want 
+    to set.
+*/
 
 window.AppRefs = {
+    APIKEY: '',
     UserOptions: {
-
+        UserID: '',
+        TargetAPI: {
+            host: '',
+            headers: []
+        }
     }
 };
 
 if (localStorage) {
     const UserOptions = localStorage.getItem('UserOptions');
-    if(UserOptions){
+    if (UserOptions) {
         window.AppRefs.UserOptions = UserOptions;
     }
 }
