03c3aeb422934a5b3517fdf20357dfacb955f4e1
John Nelson <me@johnrnelson.com>
2019-04-23 14:36:15 -0700

    :eyes: UI updates

--- docs/debug/src/AppPrefs.js
+++ docs/debug/src/AppPrefs.js
@@ -8,26 +8,85 @@ window.AppRefs = {
     UserOptions: {
         UserID: '',
         TargetAPI: {
-            url: document.URL ,
-            opts:{
-                favor:'put'
+            url: document.URL,
+            opts: {
+                favor: 'put'
             }
         }
     },
-    UI:{
-        ShowPrefs(){
-            console.info('User--',AppRefs.UserOptions);
+    UI: {
+        ShowPrefs() {
+            console.info('TODO -->', 'Display AppRefs-->', AppRefs);
+        },
+        ClearPrefs() {
+
+
+            Metro.dialog.create({
+                title: "Clear the local storage?",
+                content: "<div>Are you sure you want to do this?</div>",
+                actions: [
+                    {
+                        caption: "Agree",
+                        cls: "js-dialog-close alert",
+                        onclick: function () {
+                            try {
+                                localStorage.clear();
+                                UIHelper.Logger.Add({
+                                    TID: 0,
+                                    Type: 707,
+                                    DT: new Date(),
+                                    Topic: "Local Storage",
+                                    Source: "Browser",
+                                    Body: "The local storage has been cleared!",
+                                });
+                            } catch (errLocalStorage) {
+                                UIHelper.Logger.Add({
+                                    TID: 0,
+                                    Type: 0,
+                                    DT: new Date(),
+                                    Topic: "Local Storage",
+                                    Source: "Browser",
+                                    Body: errLocalStorage.message
+                                });
+                            }
+
+
+                        }
+                    },
+                    {
+                        caption: "Disagree",
+                        cls: "js-dialog-close"
+                    }
+                ]
+            });
+
+
+
+
+
+
+
+
+
+
+
+
         }
     },
 };
 
 if (localStorage) {
-    const UserOptions = localStorage.getItem('UserOptions');
-    if (UserOptions) {
-        window.AppRefs.UserOptions = UserOptions;
+    const UserOptionsText = localStorage.getItem('UserOptions');
+
+    if (UserOptionsText) {
+        console.info('Reloading App options....');
+        window.AppRefs.UserOptions = JSON.parse(UserOptionsText);
+    } else {
+        console.info('Storing (local) App options....');
+        localStorage.setItem("UserOptions", JSON.stringify(window.AppRefs.UserOptions));
     }
 
-    console.info('App options have loaded....');
+
 
 } else {
     console.Warn('No Local Storage!');
