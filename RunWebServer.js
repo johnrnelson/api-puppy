c2f2028f422c8c2352594d7523212c9ef5bd013b
John Nelson <me@johnrnelson.com>
2019-04-28 16:03:23 -0700

    ver bump

--- RunWebServer.js
+++ RunWebServer.js
@@ -25,7 +25,7 @@ const path = require('path');
 global.SERVER = {
     ProjectInfo: {
         Title: 'api-puppy Demo',
-        Version: '1.10.37',
+        Version: '1.10.38',
     },
     Network: {
         /* 
