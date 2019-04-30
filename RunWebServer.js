ffd836cd5d7b6620379b8ff3db8a05cb35cd9015
John Nelson <me@johnrnelson.com>
2019-04-30 13:30:54 -0700

    ready for push

--- RunWebServer.js
+++ RunWebServer.js
@@ -25,7 +25,7 @@ const path = require('path');
 global.SERVER = {
     ProjectInfo: {
         Title: 'api-puppy Demo',
-        Version: '1.10.4.13',
+        Version: '1.10.4.14',
         VersionName: 'Dull-Nail'
     },
     Network: {
