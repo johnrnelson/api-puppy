6d49cb09c61ac01b6578175e8ba30ddeecd92d4e
John Nelson <me@johnrnelson.com>
2019-04-30 09:23:11 -0700

    hiding displays until needed...

--- RunWebServer.js
+++ RunWebServer.js
@@ -25,7 +25,7 @@ const path = require('path');
 global.SERVER = {
     ProjectInfo: {
         Title: 'api-puppy Demo',
-        Version: '1.10.4.4',
+        Version: '1.10.4.5',
         VersionName: 'Dull-Lemon'
     },
     Network: {
