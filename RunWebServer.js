6b6a392540ae9529ac2fb89ac471aa120de6b370
John Nelson <me@johnrnelson.com>
2019-05-01 09:10:37 -0700

    pack testing

--- RunWebServer.js
+++ RunWebServer.js
@@ -13,7 +13,9 @@
 
 const fs = require('fs');
 const path = require('path');
+const api_puppy = require('./index');
 
+const package_data = JSON.parse(fs.readFileSync("./package.json", 'utf8'));
 
 
 /*
@@ -23,10 +25,8 @@ const path = require('path');
 */
 
 
-var package_data = JSON.parse(fs.readFileSync("./package.json", 'utf8'));
-
-
 global.SERVER = {
+ 
     ProjectInfo: {
         Title: 'api-puppy Demo',
         Version: package_data.version,
@@ -103,11 +103,7 @@ global.SERVER = {
         },
     },
 
-
-
-
-
-
+ 
 
 
     /*
@@ -129,58 +125,14 @@ global.SERVER = {
         fruit: ['apples', 'pears', 'peaches']
     }
 };
+ 
+
+
+api_puppy({
+    MyOptions: 4
+});
 
 
 
 
-//Load up our defender...
-SERVER.Defender = require("./APIServer/defender/shield");
-
-//The born date of this process...
-SERVER.Started = new Date();
-
-
-//Get our server options...
-(function () {
-    try {
-
-        const APIServerOptions = JSON.parse(fs.readFileSync(path.join(__dirname, "SECRET", "CONFIG.json"), "utf8"));
-        console.log("Setting API Options via the CONFIG File");
-        SERVER.CERTS = APIServerOptions.CERTS;
-        SERVER.KEYS = APIServerOptions.KEYS;
-
-        SERVER.ServiceLogger = require('./APIServer/ServiceLogger');
-        SERVER.ServiceLogger.SetOptions(APIServerOptions.Logger);
-
-
-
-    } catch (err) {
-        console.log('Please fix your CONFIG.json file in the "SERCRET" folder!');
-        process.exit(1);
-    }
-})();
-
-
-
-
-/*
-    Save time by just compiling the debug files.. You can always make this a 
-    task or use grunt or whatever ya like... :-)
-
-    Use the sync since we won't bother starting up the http servers untill
-    we have the right debug html.. 
-*/
-(function () {
-    const path2debug = __dirname + "/docs/debug/";
-
-    var debugHTML = fs.readFileSync(path2debug + "src/debug.html", 'utf8');
-    const clientjs = fs.readFileSync(path2debug + "src/client.js", 'utf8');
-
-    debugHTML = debugHTML.replace('/* SERVER REPLACES SCRIPTS */', clientjs);
-    fs.writeFileSync(path2debug + 'min/debug.html', debugHTML, { flag: 'w' });
-})();
-
-
-const APIServer = require('./APIServer/APIServer');
-APIServer.StartServer();
 
--- index.js
+++ index.js
@@ -1,3 +1,72 @@
 /*
     PREP for packaging!
 */
+
+const fs = require('fs');
+const path = require('path');
+
+
+module.exports = function (ConfigOptoins) {
+    
+
+
+    //Load up our defender...
+    SERVER.Defender = require("./APIServer/defender/shield");
+
+    //The born date of this process...
+    SERVER.Started = new Date();
+
+
+    //Get our server options...
+    (function () {
+        try {
+
+
+            // debugger;
+            const APIServerOptions = JSON.parse(fs.readFileSync(path.join(SERVER.SECRET, "CONFIG.json"), "utf8"));
+            console.log("Setting API Options via the CONFIG File");
+            SERVER.CERTS = APIServerOptions.CERTS;
+            SERVER.KEYS = APIServerOptions.KEYS;
+
+            SERVER.ServiceLogger = require('./APIServer/ServiceLogger');
+            SERVER.ServiceLogger.SetOptions(APIServerOptions.Logger);
+
+
+
+        } catch (err) {
+            console.log('Please fix your CONFIG.json file in the "SERCRET" folder!');
+            process.exit(1);
+        }
+    })();
+
+
+
+
+
+
+    /*
+        Save time by just compiling the debug files.. You can always make this a 
+        task or use grunt or whatever ya like... :-)
+    
+        Use the sync since we won't bother starting up the http servers untill
+        we have the right debug html.. 
+    */
+    (function () {
+     
+        const path2debug = __dirname + "/docs/debug/";
+
+        var debugHTML = fs.readFileSync(path2debug + "src/debug.html", 'utf8');
+        const clientjs = fs.readFileSync(path2debug + "src/client.js", 'utf8');
+
+        debugHTML = debugHTML.replace('/* SERVER REPLACES SCRIPTS */', clientjs);
+        fs.writeFileSync(path2debug + 'min/debug.html', debugHTML, { flag: 'w' });
+    })();
+
+
+    const APIServer = require('./APIServer/APIServer');
+    APIServer.StartServer();
+
+
+
+}
+
