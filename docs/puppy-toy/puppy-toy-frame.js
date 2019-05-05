6c038b44663c3f3f3ca00fa363fd55e9ca272bf0
John Nelson <me@johnrnelson.com>
2019-05-05 09:11:19 -0700

    using frame

---
+++ docs/puppy-toy/puppy-toy-frame.js
@@ -0,0 +1,402 @@
+/*
+    This script file gets loaded into the iframe.
+*/
+
+console.log("puppy-toy-frame.js");
+
+const pupframe = {
+
+    LoadCSSLink(HREF2CSS) {
+        const elLink = document.createElement("link");
+        elLink.rel = "stylesheet";
+        elLink.type = "text/css";
+        elLink.href = HREF2CSS;
+        // document.head.appendChild(elLink);
+        // debugger;
+        document.head.appendChild(elLink);
+    },
+    xhr(VERB, ROUTE, SENDMSG, OnData, OnError) {
+
+        var xhttp = new XMLHttpRequest();
+
+
+        xhttp.onreadystatechange = function () {
+
+            if (this.readyState == 4 && this.status == 200) {
+                if (this.responseText.length) {
+                    try {
+                        const srvData = JSON.parse(this.responseText);
+                        OnData(srvData);
+                    } catch (errBadJSON) {
+                        OnData({
+                            err: "Bad JSON!",
+                            body: this.responseText
+                        });
+
+                    }
+
+                } else {
+                    OnData({
+                        err: 'xhr empty data!',
+                        v: VERB,
+                        r: ROUTE,
+                        s: SENDMSG
+                    });
+
+                }
+            }
+
+
+        };
+        xhttp.onerror = function () {
+            if (!OnError) {
+                console.warn('Error XHR:' + VERB + ':' + ROUTE + ' ST:' + xhttp.status);
+                debugger;
+            } else {
+                OnError({
+                    VERB: VERB,
+                    ROUTE: ROUTE,
+                });
+            }
+            console.log("** An error occurred during the transaction");
+        };
+
+        xhttp.open(VERB, ROUTE, true);
+
+        // CORS stuff...       
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+        xhttp.setRequestHeader("Access-Control-Allow-Headers", "*");
+
+        try {
+            //Trying to trap the network errors?
+            xhttp.send(JSON.stringify(SENDMSG));
+
+        } catch (e) {
+            debugger;
+            console.log('gesh');
+        }
+    },
+    //=======
+    UI: {
+        HostFrame: null,
+        //This is our main hosting element...
+        HostElement: null,
+        HostElementMenu: null,
+        AddResultRow(HTMLElement) {
+            if (!HTMLElement.innerText) {
+                return;
+            }
+
+            const ResultSearchBody = pupframe.UI.HostElement.querySelector('ResultsSearch tbody');
+
+            const newRow = document.createElement('tr');
+            newRow.innerHTML = `
+                <tr>
+                    <td>
+                        <i class="fas fa-plus-circle"></i>
+                        <i class="fas fa-minus-circle"></i>
+                    
+                    </td>            
+                    <td>
+                        ${HTMLElement.nodeName}
+                    </td>
+                    <td>
+                        ${HTMLElement.innerText}
+                    </td>
+                </tr>            
+            `;
+            ResultSearchBody.appendChild(newRow);
+
+        },
+
+        Displays: {
+            ActiveDisplay: false,
+            ShowDisplay(ShowDisplayID) {
+                if (pupframe.UI.Displays.ActiveDisplay) {
+                    pupframe.UI.Displays.ActiveDisplay.style.display = "none";
+                }
+                pupframe.UI.Displays.ActiveDisplay = pupframe.UI.HostElement.querySelector('#tab-' + ShowDisplayID);
+                pupframe.UI.Displays.ActiveDisplay.style.display = "block";
+            },
+            MainHelp: {
+                Build() {
+
+                    //BUild the first tab!!!
+                    (function () {
+                        const HTML = `
+                    <br><br>
+                    <center>
+                       Simple help
+                    </center> 
+                       
+                    `;
+                        const newTab = document.createElement('disptab');
+                        newTab.style.display = "none";
+                        newTab.id = "tab-info";
+                        newTab.innerHTML = HTML;
+
+                        pupframe.UI.HostElement.appendChild(newTab);
+                    })();
+
+
+
+                }
+            },
+            Search: {
+                Build() {
+
+                    //BUild the first tab!!!
+                    (function () {
+
+                        const HTML = `                        
+                            <qrybox>
+                                <span class="label">Search:</span>
+                                <span class="spantext"> 
+                                    <input type="text" id="qryTextValue" value="" placeholder="Enter Query String"/>
+                                </span>
+                            </qrybox>
+                            <ResultStats>
+                                <statgroup>
+                                    <stlabel><i class="fas fa-code"></i> Total Tags</stlabel>                    
+                                    <stvalue id="TotalTags">N/A</stvalue>
+                                </statgroup>
+                                <statgroup>
+                                    <stlabel><i class="fas fa-cubes"></i> Total JSON Objects</stlabel>                    
+                                    <stvalue id="TotalJSONOBjects">N/A</stvalue>
+                                </statgroup>                        
+                            </ResultStats>
+
+                            <ResultsSearch>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <td width="20px">Options</td>                          
+                                            <td width="30px">Tage Name</td>                           
+                                            <td width="100%">Text Value</td>
+                                        <tr>                                                        
+                                    <thead>
+                                    <tbody>                         
+                                    </tbody>
+                                </table>
+                            </ResultsSearch> 
+                       
+                        `;
+                        const newTab = document.createElement('disptab');
+                        newTab.style.display = "none";
+                        newTab.id = "tab-search";
+                        newTab.innerHTML = HTML;
+
+                        pupframe.UI.HostElement.appendChild(newTab);
+                    })();
+
+
+
+
+                    //Do the document query..
+                    const qryTextValue = pupframe.UI.HostElement.querySelector('#qryTextValue');
+
+
+                    qryTextValue.QueryElement = function () {
+
+                        const srchVal = this.value.trim();
+
+                        if (!srchVal) {
+                            return;
+                        }
+
+                        console.info('Searching document for :' + srchVal);
+                        pupframe.UI.HostElement.querySelector('ResultsSearch tbody').innerHTML = "";
+
+                        // Make sure you do a full body search! :-) 
+                        const qryTags = document.body.querySelectorAll(srchVal);
+
+                        console.log('Total Tags Found:', qryTags);
+
+                        const TotalTags = pupframe.UI.HostElement.querySelector('stvalue#TotalTags');
+                        const TotalJSONOBjects = pupframe.UI.HostElement.querySelector('stvalue#TotalJSONOBjects');
+
+
+                        TotalTags.innerHTML = "" + qryTags.length + "";
+                        TotalJSONOBjects.innerHTML = "N/A";
+
+                        for (let index = 0; index < qryTags.length; index++) {
+                            const qryElement = qryTags[index];
+                            pupframe.UI.AddResultRow(qryElement);
+
+                        }
+                    };
+
+
+                    qryTextValue.addEventListener("change", function (eventinfo) {
+                        this.QueryElement();
+                    });
+                    // setTimeout(() => {
+                    //     qryTextValue.focus();
+                    // }, 50);                    
+
+                }
+            },
+            Config: {
+                CheckServer() {
+                    // pupframe.UI.Displays.Config.CheckServer();
+
+                    pupframe.xhr('PUT', 'https://demo.tektology.com/', {
+                        service: 'help',
+                        data: {
+                            topic: 'SysInfo'
+                        }
+                    }, function (ServerResponse) {
+                        console.info('DEMO SERVER INFO', ServerResponse);
+                    });
+
+                },
+                Build() {
+
+                    //BUild the first tab!!!
+                    (function () {
+                        const HTML = `
+                        <br><br>
+                        <center>
+                           Basic app options
+                        </center>
+                        <button onclick="window.parent.pupframe.UI.Displays.Config.CheckServer();">test</button>
+                       
+                    `;
+                        const newTab = document.createElement('disptab');
+                        newTab.style.display = "none";
+                        newTab.id = "tab-config";
+                        newTab.innerHTML = HTML;
+
+                        pupframe.UI.HostElement.appendChild(newTab);
+                    })();
+
+
+
+                }
+            },
+        },
+
+
+
+        BuildUIDisplay() {
+
+
+            //Build the Menu...
+            (function () {
+              
+            })();
+
+
+
+
+
+
+
+
+
+
+            var hostEl =  document.body.appendChild(document.createElement('JSONThiefDisplay'));
+            // var hostEl = document.body.appendChild(document.createElement('JSONThiefDisplay'));
+            // hostEl.style.display = "none";
+            pupframe.UI.HostElement = hostEl;
+
+
+
+            //Setup Main display and side opts...
+            (function () {
+
+                const HTML = `                
+           
+                <sidebaropts>                        
+                    <icon class="fas fa-info-circle" id="info"></icon> 
+                    <icon class="fas fa-search" id="search"></icon> 
+                    <icon class="fas fa-cogs" id="config"></icon>                         
+                    <hr/ width="70%" size="1" color="silver">
+                    <icon class="fas fa-times-circle" id="close"></icon> 
+                </sidebaropts>
+
+            `;
+                pupframe.UI.HostElement.innerHTML = HTML;
+
+
+                //Setup events!
+                var el_close = pupframe.UI.HostElement.querySelector('sidebaropts icon#close');
+                el_close.onclick = function () {
+                    window.parent.puppytoy.ToggleMenu();
+                };
+                var el_info = pupframe.UI.HostElement.querySelector('sidebaropts icon#info');
+                el_info.onclick = function () {
+                    pupframe.UI.Displays.ShowDisplay(this.id);
+                };
+                var el_search = pupframe.UI.HostElement.querySelector('sidebaropts icon#search');
+                el_search.onclick = function () {
+                    pupframe.UI.Displays.ShowDisplay(this.id);
+                };
+                var el_config = pupframe.UI.HostElement.querySelector('sidebaropts icon#config');
+                el_config.onclick = function () {
+                    pupframe.UI.Displays.ShowDisplay(this.id);
+                };
+
+
+            })();
+
+
+            pupframe.UI.Displays.MainHelp.Build();
+            pupframe.UI.Displays.Search.Build();
+            pupframe.UI.Displays.Config.Build();
+
+
+        },
+        BuildHostFrame() {
+
+        }
+    },
+    /*
+        Lets get this part started!
+    */
+    Init() {
+
+        // debugger;
+
+        console.info('Building UI for JSONThief');
+        pupframe.UI.BuildUIDisplay();
+
+
+
+        //Show your default display...
+        // pupframe.UI.Displays.ShowDisplay('info');
+        pupframe.UI.Displays.ShowDisplay('search');
+        window.parent.puppytoy.ToggleMenu();
+
+        // debugger;
+        // console.warn('take me out unless debugging!')
+        // pupframe.UI.HostElement.querySelector('#qryTextValue').QueryElement();
+
+    }
+};
+
+
+
+/*
+    Load your styles you need. It will go in the iframe not the host document!!!!
+*/
+pupframe.LoadCSSLink("https://use.fontawesome.com/releases/v5.8.1/css/all.css");
+pupframe.LoadCSSLink("https://fonts.googleapis.com/css?family=Abel");
+pupframe.LoadCSSLink("https://fonts.googleapis.com/css?family=PT+Sans:400,400italic");
+pupframe.LoadCSSLink("https://fonts.googleapis.com/css?family=Roboto+Condensed:300");
+
+if ((window.parent.location.hostname == "127.0.0.1") ||
+    (window.parent.location.hostname == "localhost") ||
+    (window.parent.location.hostname == "0.0.0.0")) {
+    console.info('Loading CSS from local!');
+    pupframe.LoadCSSLink("http://localhost:9080/?/puppy-toy/puppy-toy.css");
+} else {
+    console.info('Loading CSS from "demo.tektology.com"!');
+    pupframe.LoadCSSLink("https://demo.tektology.com/?/puppy-toy/puppy-toy.css");
+}
+
+
+
+pupframe.Init();
+
---
+++ docs/puppy-toy/puppy-toy.js
@@ -0,0 +1,183 @@
+/*
+    This file runs in the main page context. It's added directly into the DOM.
+*/
+(function () {
+    /*
+    window.parent.puppytoy
+    */
+
+    window.puppytoy = {
+        DocumentJSON: [],
+        UI:{},
+        SniffElement(ParentSniffElement, JSONData) {
+
+            JSONData.items = [];
+
+
+            for (var el in ParentSniffElement.childNodes) {
+                const element = ParentSniffElement.childNodes[el];
+
+                //Skip nodes that don't have a tag name...
+                if (!element.nodeName) {
+                    continue;
+                }
+
+                /*
+                    Skip certain elements...
+                */
+                if (
+                    (element.nodeName == "JSONThiefDisplay") ||
+                    (element.nodeName == "#comment") ||
+                    (element.nodeName == "#text") ||
+                    (element.nodeName == "SCRIPT") ||
+                    (element.nodeName == "BR") ||
+                    (element.nodeName == "img")) {
+                    continue;
+                }
+
+
+                var elData = {
+                    tag: element.nodeName
+                };
+                JSONData.items.push(elData);
+
+                if (element.nodeName == "P") {
+                    elData.text = element.innerText;
+                    continue;
+                }
+
+
+
+
+                // if (element.innerText == "nested") {
+                //     debugger;
+                // }
+                if (element.childNodes.length == 1) {
+                    //Pick up the text of the element...
+                    if (element.childNodes[0].nodeName == "#text") {
+                        elData.text = element.innerText;
+                    } else {
+                        debugger;
+                    }
+                } else {
+                    // puppytoy.SniffElement(element, elData);
+                }
+            }
+
+
+
+        },
+        BuildMenu(){
+            var hostElMenu = document.body.appendChild(document.createElement('JSONThieMenu'));
+            // <i class="fas fa-sitemap"></i>
+            hostElMenu.innerHTML = "*";
+
+            hostElMenu.style.position = "fixed";
+            hostElMenu.style.display = "block";
+
+            hostElMenu.style.zIndex = "5000";
+            hostElMenu.style.margin = "0";
+
+            hostElMenu.style.padding = "3px";
+
+            hostElMenu.style.top = "25px";
+            hostElMenu.style.right = "25px";
+
+            // hostElMenu.style.width = "40px";
+            hostElMenu.style.cursor = "hand";
+            hostElMenu.style.textOverflow = "default";
+            hostElMenu.style.backgroundColor = "rgb(56, 56, 56)";
+            hostElMenu.style.borderRadius = "20px";
+            hostElMenu.style.fontSize = "30px";
+            hostElMenu.style.textShadow = "1px 1px 1px rgb(253, 253, 253)";
+            hostElMenu.style.color = "gold";
+            hostElMenu.style.textAlign = "center";
+            // debugger;
+
+
+
+            hostElMenu.onclick = function (evt, others) {
+                puppytoy.ToggleMenu();
+            };
+
+            puppytoy.UI.HostElementMenu = hostElMenu;            
+        },
+        ToggleMenu() {
+            if (puppytoy.UI.HostElementMenu.style.display == "none") {
+                puppytoy.UI.HostElementMenu.style.display = "";
+                // puppytoy.UI.HostElement.style.display = "none";
+                puppytoy.UI.HostFrameContainer.style.display = "none";
+            } else {
+                puppytoy.UI.HostElementMenu.style.display = "none";
+                // puppytoy.UI.HostElement.style.display = "";
+                puppytoy.UI.HostFrameContainer.style.display = "";
+            }
+        },
+    };
+
+
+    window.puppytoy.BuildMenu();
+
+
+
+
+
+    (function () {
+
+
+
+        puppytoy.UI.HostFrameContainer = document.createElement('IFRAMEHOST');
+        puppytoy.UI.HostFrameContainer.style.display = "none";
+        puppytoy.UI.HostFrameContainer.style.position = "fixed";
+
+        puppytoy.UI.HostFrameContainer.style.zIndex = "5000";
+        puppytoy.UI.HostFrameContainer.style.margin = "0";
+        puppytoy.UI.HostFrameContainer.style.padding = "0";
+        puppytoy.UI.HostFrameContainer.style.top = "0";
+        puppytoy.UI.HostFrameContainer.style.left = "0";
+        puppytoy.UI.HostFrameContainer.style.right = "0";
+        puppytoy.UI.HostFrameContainer.style.bottom = "0";
+        puppytoy.UI.HostFrameContainer.style.backgroundColor = "gray";
+
+        puppytoy.UI.HostFrame = document.createElement('iframe');
+
+        // This will make contentDocument unavailable!!!!
+        // puppytoy.UI.HostFrame.src = "http://localhost:9080/?/puppy-toy/puppy-toy.html";
+
+        puppytoy.UI.HostFrame.style.cssText = "width:100%;height:100%;margin:0;padding:0;border:0";
+
+
+
+        puppytoy.UI.HostFrameContainer.appendChild(puppytoy.UI.HostFrame);
+
+        document.body.appendChild(puppytoy.UI.HostFrameContainer);
+
+
+
+
+
+        if ((document.location.hostname == "127.0.0.1") ||
+            (document.location.hostname == "localhost") ||
+            (document.location.hostname == "0.0.0.0")) {
+            console.info('Loading "puppy-toy-frame.js" from local!');
+            const CSSFile = puppytoy.UI.HostFrame.contentDocument.createElement("script");
+            CSSFile.src = "http://localhost:9080/?/puppy-toy/puppy-toy-frame.js";
+            puppytoy.UI.HostFrame.contentDocument.head.appendChild(CSSFile);
+
+        } else {
+            console.info('Loading "puppy-toy-frame.js" from "demo.tektology.com"!');
+            const CSSFile = puppytoy.UI.HostFrame.contentDocument.createElement("script");
+            CSSFile.src = "https://demo.tektology.com/?/puppy-toy/puppy-toy.js";
+            puppytoy.UI.HostFrame.contentDocument.head.appendChild(CSSFile);
+        }
+
+
+
+    })();
+
+
+
+
+    
+
+})();
