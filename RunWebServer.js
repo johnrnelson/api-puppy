a9f264831c93692eb55109693e28ebfa77c4e662
John Nelson <me@johnrnelson.com>
2019-05-17 09:27:22 -0700

    Updating for new version:2019.5.17.2

--- RunWebServer.js
+++ RunWebServer.js
@@ -26,7 +26,7 @@ var MySERVER = {
     ProjectInfo: {
         Title: 'api-puppy Demo',
         Version: JSON.parse(fs.readFileSync("./package.json", 'utf8')).version,
-        VersionName: 'Slim-Down'
+        VersionName: 'Data-Pump'
     },
     Network: {
         /* 
